f2d95f0838ce7b89ffe2416c4811a3306aa6cbfc
HildePols <atschpe@gmail.com>
2018-06-27 11:54:46 +0200

    minor correction to ListPref
    * added missing attributes needed.
    * NumberPickerPref issues still to be sorted out

--- app/src/main/java/com/example/android/medjour/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/android/medjour/ui/settings/SettingsFragment.java
@@ -1,14 +1,38 @@
 package com.example.android.medjour.ui.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.example.android.medjour.R;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener{
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.menu_settings);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // unregister the preference change listener
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // register the preference change listener
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 
     }
 }
